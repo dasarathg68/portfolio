326bff0e813138d6d6401add1e5c8cd26e75806d
Dasarath G <dasarathg68@gmail.com>
2024-04-02 08:04:57 +0530

    fix: small screen navbar menu update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,15 +22,10 @@
           tabindex="0"
           class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
+          <li><a>Home</a></li>
+          <li><a>Services</a></li>
+          <li><a>Portfolio</a></li>
+          <li><a>Resume</a></li>
         </ul>
       </div>
       <div class="dropdown dropdown-end">
