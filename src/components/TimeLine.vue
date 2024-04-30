35d9eb671a52e34590c7b9cf877e7c8e09eef8fa
Dasarath G <dasarathg68@gmail.com>
2024-04-30 20:30:31 +0530

    feat: create about page, timeline

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="mt-16">
+    <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+      <li>
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic">June 2019 - June 2023</time>
+          <div className="text-lg font-black">
+            SASTRA University<br />
+            B.Tech. Electronics & Communication
+          </div>
+          CGPA: 8.3575/10 <br />
+          Dean’s Merit List (Top 3-10% in the university) in the academic year 2019-2020 <br />
+          Finalist in Evathon 2021, University of Limerick, Ireland <br />
+          Recipient of the Desh-Videsh scholarship
+        </div>
+        <hr />
+      </li>
+      <li>
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end md:text-start mb-10">
+          <time className="font-mono italic">April 2022 - June 2022</time>
+          <div className="text-lg font-black">
+            Embedded SW Developer Intern <br />
+            Analog Devices India, Bengaluru (Remote)
+          </div>
+          <div class="text-justify">
+            Worked as a summer intern and gained insights about BMS, embedded software and isoSPI. I
+            completed a project titled
+            <i class="font-bold">Source Meter Control Automation & Complex Device Drivers (CDD).</i>
+          </div>
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic">February 2023 - June 2023</time>
+          <div className="text-lg font-black">
+            R&D Intern<br />
+            Gerontechnology Research Center, Taiwan (Onsite)
+          </div>
+          <div class="text-justify">
+            I developed an IoT backend using Gin-Gonic in Go, managing users and devices, alongside
+            routine tasks like debugging and report writing. Additionally, I assisted in Flutter
+            feature development and built a dashboard for activity detection algorithms using smart
+            meter data API from TaiPower Company. My work contributed to our product rollout to over
+            250 customers. This experience also formed the basis of my well-received undergraduate
+            thesis, supported by scholarships including the
+            <i class="font-bold">Taiwan Experience Education Program</i> and
+            <i class="font-bold">Desh-Videsh Scholarship.</i>
+          </div>
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end md:text-start mb-10">
+          <time className="font-mono italic">April 2023 - January 2024</time>
+          <div className="text-lg font-black">
+            Lead Engineer <br />
+            bEarly TechnoVations, Chennai (Hybrid)
+          </div>
+          <div class="text-justify">
+            I played a key role in developing paceB, a biomedical device for early-stage Parkinson's
+            disease diagnosis. Utilizing ESP32, AWS, and EMQX broker, I optimized real-time data
+            streaming/storage by 13x. I built a patient management system with a payment interface
+            and IoT dashboard using Node.js, Next.js, Razorpay, MySQL & EMQX. Validated and scaled
+            the prototype through clinical studies, led website development, published a patent
+            titled
+            <i class="font-bold">A Device for Detection and Prognosis of Pathological Gait</i>,
+            co-authored a white paper, and managed team members/interns. Our efforts led to a
+            successful product launch, a milestone for bEarly.
+          </div>
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic">January 2024 - Present</time>
+          <div className="text-lg font-black">Independent Contractor<br /></div>
+          <div class="text-justify">
+            I've completed more than 10 projects covering IoT, Web3, Full Stack Development, and
+            patent writing. With a perfect Job Success Score of 100% on Upwork, I bring a track
+            record of delivering quality work.
+          </div>
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end md:text-start mb-10">
+          <time className="font-mono italic">March 2024 - Present</time>
+          <div className="text-lg font-black">
+            Web3 Developer <br />
+            Globe & Citizen, Canada (Remote)
+          </div>
+          <div class="text-justify">
+            Building the CNC (Crypto Native Corporation) Portal, an app that creates a mechanism to
+            financially acknowledge the micro contributions of Open Source collaborators along with
+            tools that promote effective governance. I'm in charge of crafting user-friendly
+            interfaces with Vue.js/Tailwind/DaisyUI/Pinia, and handling the backend using
+            Node.js/Prisma/PostgreSQL. I utilize OpenAPI to describe APIs, Solidity for smart
+            contracts, and manage a side project involving a voting system.
+          </div>
+        </div>
+        <hr />
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup></script>
