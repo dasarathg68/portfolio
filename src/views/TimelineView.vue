ceb3b4e895dbbd0b04da6e01d2115fb8449dba63
Dasarath G <dasarathg68@gmail.com>
2024-06-30 19:56:19 +0530

    fix: styles

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,122 +1,91 @@
 <template>
   <div class="mt-24">
-    <h1 class="text-4xl font-bold text-center">Timeline</h1>
-
+    <h1 class="text-4xl font-bold text-center mb-8">Timeline</h1>
     <div class="flex justify-center">
-      <div class="w-9/12 h-1000 mt-4">
-        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-          <li>
-            <div className="timeline-start md:text-end mb-10">
-              <time className="font-mono italic">June 2019 - June 2023</time>
-              <div className="text-lg font-black">
-                SASTRA University<br />
-                B.Tech. Electronics & Communication
-              </div>
-              CGPA: 8.3575/10 <br />
-              Dean’s Merit List (Top 3-10% in the university) in the academic year 2019-2020 <br />
-              Finalist in Evathon 2021, University of Limerick, Ireland <br />
-              Recipient of the Desh-Videsh scholarship
+      <ul
+        class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical timeline-end"
+      >
+        <li v-for="item in timelineItems" :key="item.period">
+          <div :class="`timeline-${item.position} mb-10 text-${item.position}`">
+            <time class="font-mono italic text-gray-600">{{ item.period }}</time>
+            <div class="text-lg font-black mb-2">{{ item.title }}</div>
+            <div class="text-md font-semibold mb-1">{{ item.location }}</div>
+            <div class="text-justify text-gray-800">{{ item.description }}</div>
+            <div v-if="item.achievements" class="mt-2">
+              <ul class="list-disc pl-5 text-gray-700">
+                <li v-for="achievement in item.achievements" :key="achievement">
+                  {{ achievement }}
+                </li>
+              </ul>
             </div>
-            <hr />
-          </li>
-          <li>
-            <div className="timeline-end md:text-start mb-10">
-              <time className="font-mono italic">April 2022 - June 2022</time>
-              <div className="text-lg font-black">
-                Embedded SW Developer Intern <br />
-                Analog Devices India, Bengaluru (Remote)
-              </div>
-              <div class="text-justify">
-                Worked as a summer intern and gained insights about BMS, embedded software and
-                isoSPI. I completed a project titled
-                <i class="font-bold"
-                  >Source Meter Control Automation & Complex Device Drivers (CDD).</i
-                >
-              </div>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-
-            <div className="timeline-start md:text-end mb-10">
-              <time className="font-mono italic">February 2023 - June 2023</time>
-              <div className="text-lg font-black">
-                R&D Intern<br />
-                Gerontechnology Research Center, Taiwan (Onsite)
-              </div>
-              <div class="text-justify">
-                I developed an IoT backend using Gin-Gonic in Go, managing users and devices,
-                alongside routine tasks like debugging and report writing. Additionally, I assisted
-                in Flutter feature development and built a dashboard for activity detection
-                algorithms using smart meter data API from TaiPower Company. My work contributed to
-                our product rollout to over 250 customers. This experience also formed the basis of
-                my well-received undergraduate thesis, supported by scholarships including the
-                <i class="font-bold">Taiwan Experience Education Program</i> and
-                <i class="font-bold">Desh-Videsh Scholarship.</i>
-              </div>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-end md:text-start mb-10">
-              <time className="font-mono italic">April 2023 - January 2024</time>
-              <div className="text-lg font-black">
-                Lead Engineer <br />
-                bEarly TechnoVations, Chennai (Hybrid)
-              </div>
-              <div class="text-justify">
-                I played a key role in developing paceB, a biomedical device for early-stage
-                Parkinson's disease diagnosis. Utilizing ESP32, AWS, and EMQX broker, I optimized
-                real-time data streaming/storage by 13x. I built a patient management system with a
-                payment interface and IoT dashboard using Node.js, Next.js, Razorpay, MySQL & EMQX.
-                Validated and scaled the prototype through clinical studies, led website
-                development, published a patent titled
-                <i class="font-bold">A Device for Detection and Prognosis of Pathological Gait</i>,
-                co-authored a white paper, and managed team members/interns. Our efforts led to a
-                successful product launch, a milestone for bEarly.
-              </div>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-
-            <div className="timeline-start md:text-end mb-10">
-              <time className="font-mono italic">January 2024 - Present</time>
-              <div className="text-lg font-black">Independent Contractor<br /></div>
-              <div class="text-justify">
-                I've completed more than 10 projects covering IoT, Web3, Full Stack Development, and
-                patent writing. With a perfect Job Success Score of 100% on Upwork, I bring a track
-                record of delivering quality work.
-              </div>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-
-            <div className="timeline-end md:text-start mb-10">
-              <time className="font-mono italic">March 2024 - Present</time>
-              <div className="text-lg font-black">
-                Web3 Developer <br />
-                Globe & Citizen, Canada (Remote)
-              </div>
-              <div class="text-justify">
-                Building the CNC (Crypto Native Corporation) Portal, an app that creates a mechanism
-                to financially acknowledge the micro contributions of Open Source collaborators
-                along with tools that promote effective governance. I'm in charge of crafting
-                user-friendly interfaces with Vue.js/Tailwind/DaisyUI/Pinia, and handling the
-                backend using Node.js/Prisma/PostgreSQL. I utilize OpenAPI to describe APIs,
-                Solidity for smart contracts, and manage a side project involving a voting system.
-              </div>
-            </div>
-            <hr />
-          </li>
-        </ul>
-      </div>
+          </div>
+          <hr />
+        </li>
+      </ul>
     </div>
   </div>
 </template>
-<script setup></script>
+
+<script setup>
+const timelineItems = [
+  {
+    period: 'June 2019 - June 2023',
+    title: 'SASTRA University',
+    location: 'B.Tech. Electronics & Communication',
+    description: 'CGPA: 8.3575/10',
+    achievements: [
+      'Dean’s Merit List (Top 3-10% in the university) in the academic year 2019-2020',
+      'Finalist in Evathon 2021, University of Limerick, Ireland',
+      'Recipient of the Desh-Videsh scholarship'
+    ],
+    position: 'start'
+  },
+  {
+    period: 'April 2022 - June 2022',
+    title: 'Embedded SW Developer Intern',
+    location: 'Analog Devices India, Bengaluru (Remote)',
+    description:
+      "Worked as a summer intern and gained insights about BMS, embedded software, and isoSPI. I completed a project titled 'Source Meter Control Automation & Complex Device Drivers (CDD).'",
+    position: 'end'
+  },
+  {
+    period: 'February 2023 - June 2023',
+    title: 'R&D Intern',
+    location: 'Gerontechnology Research Center, Taiwan (Onsite)',
+    description:
+      'Developed an IoT backend using Gin-Gonic in Go, managing users and devices. Assisted in Flutter feature development and built a dashboard for activity detection algorithms. My work contributed to our product rollout to over 250 customers.',
+    achievements: [
+      'Supported by the Taiwan Experience Education Program and Desh-Videsh Scholarship'
+    ],
+    position: 'start'
+  },
+  {
+    period: 'April 2023 - January 2024',
+    title: 'Lead Engineer',
+    location: 'bEarly TechnoVations, Chennai (Hybrid)',
+    description:
+      "Developed paceB, a biomedical device for early-stage Parkinson's diagnosis. Optimized real-time data streaming by 13x. Built a patient management system with payment interface and IoT dashboard.",
+    achievements: [
+      "Published a patent titled 'A Device for Detection and Prognosis of Pathological Gait'",
+      'Co-authored a white paper and led team members/interns'
+    ],
+    position: 'end'
+  },
+  {
+    period: 'January 2024 - Present',
+    title: 'Independent Contractor',
+    location: '',
+    description:
+      'Completed over 10 projects covering IoT, Web3, Full Stack Development, and patent writing. Achieved a perfect Job Success Score of 100% on Upwork.',
+    position: 'start'
+  },
+  {
+    period: 'March 2024 - Present',
+    title: 'Web3 Developer',
+    location: 'Globe & Citizen, Canada (Remote)',
+    description:
+      'Building the CNC Portal, an app for financially acknowledging micro contributions of Open Source collaborators. Crafting user-friendly interfaces and handling backend development.',
+    position: 'end'
+  }
+]
+</script>
